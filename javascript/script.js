b2fd24b9dbb1785c0364f6d3fa8ee65a50d09f8c
iloElias <murilo7456@outlook.com>
2023-10-02 15:56:41 -0300

    Adicionado tela e funcionalidades a alguns botões

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,34 @@
-alert("Hello World!");
+let buttons = document.getElementsByClassName("button")
+let resultText = document.getElementById("result-text")
+
+for (let i in buttons) {
+    if (parseInt(buttons[i].value) || buttons[i].value == "0") {
+        buttons[i].addEventListener("click", function () {
+            if (buttons[i].value == "0" && resultText.innerText.length == 0) {
+
+            } else {
+                resultText.innerText += buttons[i].value
+            }
+        })
+    } else if (buttons[i].value == "") {
+
+    }
+}
+
+document.getElementById("clear").addEventListener("click", function () {
+    if (resultText.innerText.length != 0) {
+        resultText.innerText = ""
+    }
+})
+
+document.getElementById("revert-signal").addEventListener("click", function () {
+    if (resultText.innerText.length != 0 && resultText.innerText[0] != "-") {
+        let splitText = resultText.innerText.split("")
+
+        if (parseInt(splitText[splitText.length - 1])) {
+            resultText.innerText = `-${splitText.join("")}`
+        }
+    } else {
+        resultText.innerText = resultText.innerText.replace("-", "")
+    }
+})
