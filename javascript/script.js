2b417dc55a4cdbde87a73c201cfe984ceb4e6d0f
iloElias <murilo7456@outlook.com>
2023-10-03 11:41:13 -0300

    Finalizado

--- javascript/script.js
+++ javascript/script.js
@@ -1,45 +1,70 @@
 let buttons = document.getElementsByClassName("button")
 let resultText = document.getElementById("result-text")
+let clear = document.getElementById("clear")
 
-let value1, value2, result
-let operatos = "+-*/%"
+let value1, value2
+let operatos = "+-*/"
 let operation = ""
 let firstValueInsert = false
 
 function parseOperation(val1, val2, mathOperation) {
+    let result = ""
     if (mathOperation == "+") {
-        resultText.innerText = `${val1 + val2}`.replace(".", ",")
+        result = `${(val1 + val2)}`
     } else if (mathOperation == "-") {
-        resultText.innerText = `${val1 - val2}`.replace(".", ",")
+        result = `${(val1 - val2)}`
     } else if (mathOperation == "*") {
-        resultText.innerText = `${val1 * val2}`.replace(".", ",")
+        result = `${(val1 * val2)}`
     } else if (mathOperation == "/") {
-        resultText.innerText = `${val1 / val2}`.replace(".", ",")
+        result = `${(val1 / val2)}`
     }
+
+    result = result.replace(".", ",")
+
+    if (result.includes(",")) {
+        let dotIndex = result.indexOf(",")
+
+        result = result.substring(0, dotIndex + 3)
+    }
+
+    resultText.innerText = result
 }
 
 for (let i in buttons) {
     if (parseInt(buttons[i].value) || buttons[i].value == "0") {
         buttons[i].addEventListener("click", function () {
-            if (buttons[i].value == "0" && resultText.innerText.length == 0) {
-
-            } else {
-                if (firstValueInsert) {
-                    resultText.innerText = ""
-                }
-                resultText.innerText += buttons[i].value
-                firstValueInsert = false
+            if (firstValueInsert && resultText.innerText != "") {
+                resultText.innerText = ""
             }
+            resultText.innerText += buttons[i].value
+
+            firstValueInsert = false
         })
     } else if (buttons[i].value == ",") {
         buttons[i].addEventListener("click", function () {
-            if (resultText.innerText != "" && !resultText.innerText.includes(",")) {
-                resultText.innerText += buttons[i].value
+            if (!resultText.innerText.includes(",")) {
+                if (resultText.innerText != "") {
+                    resultText.innerText += buttons[i].value
+                } else {
+                    resultText.innerText += "0" + buttons[i].value
+                }
             }
         })
-    } else if (buttons[i].value == "equals") {
+    } else if (buttons[i].value == "%") {
+        buttons[i].addEventListener("click", function () {
+            if (resultText.innerText != "") {
+                value1 = parseFloat(resultText.innerText.replace(",", ".")) / 100
+                value2 = undefined
+                operation = "*"
+
+                firstValueInsert = true
+                resultText.innerText = `${value1}`.replace(".", ",")
+            }
+        })
+    }
+    else if (buttons[i].value == "equals") {
         buttons[i].addEventListener("click", function () { // Botão para realizar o calculo
-            if (operation != "" && resultText.innerText != "") {
+            if (resultText.innerText != "") {
                 if (resultText.innerText[resultText.innerText.length - 1] == ",") {
                     resultText.innerText += "0"
                 }
@@ -50,9 +75,9 @@ for (let i in buttons) {
                     value2 = parseFloat(resultText.innerText.replace(",", "."))
                 }
 
-                if (value1 != undefined && value2 != undefined) {
+                if (value1 != undefined && value2 != undefined && operation != "") {
                     parseOperation(value1, value2, operation)
-                    value1 = parseFloat(resultText.innerText)
+                    value1 = parseFloat(resultText.innerText.replace(",", "."))
                     value2 = undefined
                     operation = ""
                 }
@@ -62,33 +87,44 @@ for (let i in buttons) {
         })
     } else if (buttons[i].value && operatos.includes(buttons[i].value)) {
         buttons[i].addEventListener("click", function () { // Botão de operador
-            if (resultText.innerText[resultText.innerText.length - 1] == ",") {
-                resultText.innerText += "0"
-            }
+            if (resultText.innerText != "") {
+                if (resultText.innerText[resultText.innerText.length - 1] == ",") {
+                    resultText.innerText += "0"
+                }
 
-            if (value1 == undefined && value2 == undefined) {
-                value1 = parseFloat(resultText.innerText.replace(",", "."))
-            } else if (value1 != undefined && value2 == undefined) {
-                value2 = parseFloat(resultText.innerText.replace(",", "."))
-            }
+                if (value1 == undefined && value2 == undefined) {
+                    value1 = parseFloat(resultText.innerText.replace(",", "."))
+                } else if (value1 != undefined && value2 == undefined) {
+                    value2 = parseFloat(resultText.innerText.replace(",", "."))
+                }
 
-            if (value1 != undefined && value2 != undefined) {
-                parseOperation(value1, value2, operation)
-                value1 = parseFloat(resultText.innerText)
-                value2 = undefined
-                operation = ""
-            }
+                if (value1 != undefined && value2 != undefined && operation != "") {
+                    parseOperation(value1, value2, operation)
+                    value1 = parseFloat(resultText.innerText.replace(",", "."))
+                    value2 = undefined
+                    operation = ""
+                }
 
-            firstValueInsert = true
-            operation = buttons[i].value
+                if (operation == "") {
+                    value2 = undefined
+                }
+
+                operation = buttons[i].value
+                firstValueInsert = true
+            }
         })
     }
-
 }
 
-document.getElementById("clear").addEventListener("click", function () {
+clear.addEventListener("click", function () {
     if (resultText.innerText.length != 0) {
         resultText.innerText = ""
+        if (value1 != undefined && value2 == undefined) {
+            clear.innerText = "AC"
+        } else if (value1 == undefined && value2 == undefined) {
+            value1 = undefined
+            clear.innerText = "C"
+        }
     }
 })
 
